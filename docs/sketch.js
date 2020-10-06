e0fe65c852b9deab3febe3e780db3a456431dbb2
Norikazu Teraguchi <norimaki.tera@gmail.com>
2020-10-06 19:53:07 +0900

    [update]

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,49 +1,60 @@
+// Copyright (c) 2019 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+Style Transfer Image Example using p5.js
+This uses a pre-trained model of The Great Wave off Kanagawa and Udnie (Young American Girl, The Dance)
+=== */
+
 $(function() {
-	$('input[type=file]').after('<div><span></span></div>');
+  $('input[type=file]').after('<div><span></span></div>');
 
-	// アップロードするファイルを選択
-	$('input[type=file]').change(function() {
-		var file = $(this).prop('files')[0];
+  // アップロードするファイルを選択
+  $('input[type=file]').change(function() {
+    var file = $(this).prop('files')[0];
 
-		// 画像以外は処理を停止
-		if (! file.type.match('image.*')) {
-			// クリア
-			$(this).val('');
-			$('span').html('');
-			return;
-		}
+    // 画像以外は処理を停止
+    if (! file.type.match('image.*')) {
+      // クリア
+      $(this).val('');
+      $('span').html('');
+      return;
+    }
 
-		// 画像表示
-		var reader = new FileReader();
-		reader.onload = function() {
-			var img_src = $('<img id="inputImg">').attr('src', reader.result);
-			$('span').html(img_src);
-		};
-		reader.readAsDataURL(file);
-	});
+    // 画像表示
+    var reader = new FileReader();
+    reader.onload = function() {
+      var img_src = $('<img id="inputImg">').attr('src', reader.result);
+      $('span').html(img_src);
+    };
+    reader.readAsDataURL(file);
+  });
 });
-
 
 let inputImg;
 let statusMsg;
 let transferBtn;
-
-// Create two Style methods with different pre-trained models
-const style1 = ml5.styleTransfer('models/wave', modelLoaded);
-const style2 = ml5.styleTransfer('models/udnie', modelLoaded);
+let style1;
+let style2;
 
 function setup() {
-
   noCanvas();
   // Status Msg
   statusMsg = select('#statusMsg');
 
   // Get the input image
-  // inputImg = select('#inputImg');
+  inputImg = select('#inputImg');
 
   // Transfer Button
   transferBtn = select('#transferBtn')
   transferBtn.mousePressed(transferImages);
+
+  // Create two Style methods with different pre-trained models
+  style1 = ml5.styleTransfer('models/wave', modelLoaded);
+  style2 = ml5.styleTransfer('models/udnie', modelLoaded);
 }
 
 // A function to be called when the models have loaded
@@ -56,19 +67,15 @@ function modelLoaded() {
 
 // Apply the transfer to both images!
 function transferImages() {
-	inputImg = select('#inputImg');
-
   statusMsg.html('Applying Style Transfer...!');
 
-  style1.transfer(inputImg, function(result) {
+  style1.transfer(inputImg, function(err, result) {
     createImg(result.src).parent('styleA');
   });
 
-  style2.transfer(inputImg, function(result) {
+  style2.transfer(inputImg, function(err, result) {
     createImg(result.src).parent('styleB');
   });
 
   statusMsg.html('Done!');
 }
-
-
